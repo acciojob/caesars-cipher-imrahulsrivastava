7f0aa737af5c86feaff488094745d3b78e2bad15
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-10-11 17:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ const lookup = {
   Z: "M",
   "?": "?",
   ",": ",",
+  " ": " ",
 };
 
 function rot13(encodedStr) {
@@ -40,7 +41,7 @@ function rot13(encodedStr) {
     decodedArr.push(lookup[ch]);
   }
 
-  return decodedArr.join(' ');
+  return decodedArr.join('');
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
